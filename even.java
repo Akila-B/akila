9368d81fd1b702be0a77b2aae9b5e926471bee59
Akila-B <akilakala97@gmail.com>
2017-06-30 09:42:00 +0530

    even.java

---
+++ even.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class even
+{
+public static void main(String[] args)
+{
+Scanner sc=new scanner(System.in);
+int a=sc.nextInt();
+if(a/2==0)
+{
+System.out.println("The number is even");
+}
+else
+{
+System.out,println("The number is odd");
+}
+}
+}
